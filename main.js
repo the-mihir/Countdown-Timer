06a4bc869be07f941e0f715b4bb7dafb876d6856
Mihir <mihirkantho@gmail.com>
2021-07-05 12:54:27 +0600

    Final

---
+++ main.js
@@ -0,0 +1,36 @@
+daysE1 = document.getElementById("days");
+hoursE1 = document.getElementById("hours");
+minsE1 = document.getElementById("mins");
+secondsE1 = document.getElementById("seconds");
+
+
+const newYears = "1 jan 2022";
+
+function countdown() {
+
+    const newYearDate = new Date (newYears);
+    const currentDate = new Date();
+    const totalSeconds = (newYearDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds/ 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysE1.innerHTML = days;
+    hoursE1.innerHTML = hours;
+    minsE1.innerHTML = mins;
+    secondsE1.innerHTML = seconds;
+}
+
+// function formatTime(time) {
+//     return time < 10 ? '0${time}' : time;
+// }
+
+
+
+
+// initial Call
+countdown();
+
+setInterval(countdown, 1000);
